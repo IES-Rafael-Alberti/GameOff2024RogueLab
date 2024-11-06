d73e5a1b6c781948da7f81b83f6f9953eec66ddc
mlanbay <lancharro356@gmail.com>
2024-11-06 13:29:48 +0100

    Canvas Manager init

---
+++ scripts/CanvasManager.gd
@@ -0,0 +1,18 @@
+extends PanelContainer
+
+var glitch_tv_effect:bool
+var vignet_effect:bool
+var vignet_effect_amount:int
+
+func glich_tv_effect_enable():
+	pass
+	
+func vignet_effect_enable(amount: int):
+	pass
+	
+func glitch_tv_disable():
+	pass
+	
+func vignet_effect_disable():
+	pass
+	
