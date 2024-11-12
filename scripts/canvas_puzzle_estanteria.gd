bac48463808cd0155d7414c86304fa31db7deb8a
Payoyo12 <clasedepoyo@gmail.com>
2024-11-13 00:38:42 +0100

    ejemplo de lo que creo k tengo k hacer

---
+++ scripts/canvas_puzzle_estanteria.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+
+
+
+func _on_button_pressed() -> void:
+	
+	print("Boton: DialogoManager.funcioncorrespondiente")
+	
+	
+
+
+func _on_button_2_pressed() -> void:
+
+
+	print("Boton 2: DialogoManager.funcioncorrespondiente")
+
+
+
+
+func _on_button_3_pressed() -> void:
+	
+	
+
+	print("Boton 3: DialogoManager.funcioncorrespondiente")
+
+
+func _on_button_4_pressed() -> void:
+	
+	
+	print("Boton 4: DialogoManager.funcioncorrespondiente")
