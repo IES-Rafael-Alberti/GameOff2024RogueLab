c87dc04d1a3146675ea0509a391132f4da6c0bbc
Payoyo12 <119692334+Payoyo12@users.noreply.github.com>
2024-11-13 13:18:51 +0100

    puzzle terminado sin contar con gameManager

--- scripts/tornillo_derecho.gd
+++ scripts/tornillo_derecho.gd
@@ -1,6 +1,9 @@
 extends Sprite2D
 @onready var animation_player = $AnimationPlayer
 
+#eliminar esto
+@onready var scene_rejilla = $"../.."
+
 # Variable para almacenar si el clic izquierdo se ha realizado
 var clicked_left: bool = false
 
@@ -10,7 +13,7 @@ func _ready():
 
 # Detectar entrada del ratón (clic izquierdo)
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and get_global_mouse_position().distance_to(global_position) < 20.0:
 		print("clikIzquierdo")  # Si se hace clic izquierdo
 		if event.pressed and get_global_mouse_position().distance_to(global_position) < 20.0:
 			clicked_left = true  # Cambia el estado a true al hacer clic
@@ -19,12 +22,22 @@ func _unhandled_input(event: InputEvent) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	if clicked_left :  # Verifica que GameManager.flag1 esté activado
+	if clicked_left and not animation_player.is_playing(): #if clicked_left and GameManager.destornillador:  # Verifica que GameManager.flag1 esté activado
+		clicked_left = false
 		animation_player.play("rotar")  # Activa la animación llamada "rotar"
-		await animation_player.animation_finished
+		
 		print("animacionejecutada")
 		#await animation_player.finished
-		clicked_left = false  # Restablece el estado después de iniciar la animación
-		queue_free()
+		  # Restablece el estado después de iniciar la animación
+		#queue_free()
 	
 	pass
+	
+
+
+
+func _on_animation_player_animation_finished(anim_name):
+	scene_rejilla.tornillosQuitados += 1
+	print("tornillos quitados: "+str(scene_rejilla.tornillosQuitados))
+	
+	queue_free() # Replace with function body.
