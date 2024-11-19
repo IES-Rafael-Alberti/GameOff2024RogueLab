0793edd187bb51e24a7f4745c8f516ebad940305
Payoyo12 <119692334+Payoyo12@users.noreply.github.com>
2024-11-19 13:18:44 +0100

    fin de clase

--- scripts/puzzle/puzzle_caja_fuerte.gd
+++ scripts/puzzle/puzzle_caja_fuerte.gd
@@ -1,11 +1,29 @@
 extends CanvasLayer
 
+@onready var resultado = $resultado
+@export var tamanioCodigo: int = 4
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	
+	#conecto la señal creada en SignalBus.gd a la funcion _on__pressed
+	SignalBus.input_caja_fuerte.connect(_on__pressed) 
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	print(resultado.text)
 	pass
+
+
+
+
+func _on__pressed(numero):
+	print(numero)
+	if resultado.text.length() == tamanioCodigo:
+		pass
+	else:
+		resultado.text += numero
+#
