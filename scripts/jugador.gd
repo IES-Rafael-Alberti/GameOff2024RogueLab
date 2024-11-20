2096ec6552d439381cf4eb3635589f23d6d35b2b
mlanbay <lancharro356@gmail.com>
2024-11-20 08:30:01 +0100

    commit

--- scripts/jugador.gd
+++ scripts/jugador.gd
@@ -64,7 +64,7 @@ func _physics_process(delta):
 		
 			
 		# Registra la última entrada cuando se detecta un cambio en alguna dirección
-		if Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right"):
+		if Input.is_action_pressed("left") or Input.is_action_just_pressed("right"):
 			ultima_direccion = "horizontal"
 		elif Input.is_action_just_pressed("up") or Input.is_action_just_pressed("down"):
 			ultima_direccion = "vertical"
