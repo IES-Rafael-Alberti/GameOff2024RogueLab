7c6f340a469111816c8c960c5a05582a6a6412d1
mlanbay <lancharro356@gmail.com>
2024-11-22 10:12:05 +0100

    Puzzle fotos

--- scripts/puzzle/puzzle_caja_fuerte.gd
+++ scripts/puzzle/puzzle_caja_fuerte.gd
@@ -7,21 +7,33 @@ const PISTOLA_DINERO = preload("res://assets/sprites/Puzles/caja fuerte/pistola_
 @onready var caja_fuerte_abierta = $cajaFuerteAbierta
 @onready var resultado = $resultado
 @export var tamanioCodigo: int = 4
-var codigo: String = GameManager.codigoCajaFuerte
 
+@export var event_id_success=""
+@export var event_id_failure=""
+
+var codigo: String = GameManager.codigoCajaFuerte
+var onceProcess = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	#conecto la señal creada en SignalBus.gd a la funcion _on__pressed
 	SignalBus.input_caja_fuerte.connect(_on__pressed) 
-	
+	SignalBus.wait_input.connect(_on_input_recived)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#print(resultado.text)
+	if GameManager.puzzleLayer==self:
+		if !GameManager.DialogVisible and !GameManager.zoomItem and !onceProcess:
+			onceProcess=true
+			SignalBus.input_required.emit()
 	pass
 
+func _on_input_recived():
+	if GameManager.puzzleLayer==self:
+		if !GameManager.DialogVisible and !GameManager.zoomItem:
+			SignalBus.puzzle_exit.emit(self)
+	pass
 
 
 #funcion para cada tecla
@@ -32,8 +44,10 @@ func _on__pressed(character):
 	elif character == "ENTER":#comprueva el resultado
 		comprobarCodigo(resultado.text)
 	elif character == "PISTOLA":#interactua con la pistola
+		SignalBus.execute_event.emit(event_id_success,true)
 		CFpistola.queue_free()#eliminamos el boton
 		GameManager.caja_fuerte = true
+		onceProcess=false
 		SignalBus.zoom_item.emit(PISTOLA_DINERO,200,4,100)
 		#pistola.visible = true#muestra la puistola obtenida
 		##fin de scene
@@ -49,7 +63,8 @@ func comprobarCodigo(resultado: String):
 	if resultado == codigo:
 		caja_fuerte_abierta.visible = true
 		print("conseguido")
-		
 	else:
 		print("Intentalo de nuevo")
+		SignalBus.execute_event.emit(event_id_failure,true)
+		onceProcess=false
 		_on__pressed("EXIT")
--- scripts/puzzle/puzzle_fotos_estanteria.gd
+++ scripts/puzzle/puzzle_fotos_estanteria.gd
@@ -7,6 +7,11 @@ const FOTO_ROTA_TRASERA = preload("res://assets/sprites/Puzles/puzle4_fotos/foto
 
 var canExit=true
 
+@onready var fotos_estanterias_vacia: TextureRect = $FotosEstanteriasVacia240x135
+@onready var trasera_button: TextureButton = $TraseraButton
+
+
+
 @export var event_id=""
 
 @export var event_id_foto_fran=""
@@ -20,6 +25,8 @@ var canExit=true
 func _ready() -> void:
 	SignalBus.wait_input.connect(_on_input_recieved)
 	SignalBus.exit_zoom_item.connect(_on_zoom_out)
+	trasera_button.hide()
+	fotos_estanterias_vacia.hide()
 	pass 
 
 func _on_zoom_out(texture):
@@ -61,5 +68,13 @@ func _on_input_recieved():
 func _on_esquina_rota_pressed() -> void:
 	print("Rota trasera "+event_id)
 	SignalBus.zoom_item.emit(FOTO_ROTA_TRASERA,900,100,1000)
-	SignalBus.execute_event.emit(event_id_foto_rota_trasera,true)
+	SignalBus.execute_event.emit(event_id_foto_rota,true)
+	
+	trasera_button.show()
+	fotos_estanterias_vacia.show()
+	
+	pass # Replace with function body.
+
+
+func _on_trasera_button_pressed() -> void:
 	pass # Replace with function body.
